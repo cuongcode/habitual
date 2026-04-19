d844b6cb89e2c87ff345ea95487b837ddad72dc8
cuongcode <congcuongnguyen.dev@gmail.com>
2026-04-19 20:50:29 +0700

    increase long press

--- src/hooks/useLongPress.ts
+++ src/hooks/useLongPress.ts
@@ -1,6 +1,6 @@
 import { useRef } from 'react'
 
-export function useLongPress(onLongPress: () => void, delay = 500) {
+export function useLongPress(onLongPress: () => void, delay = 800) {
   const timerRef = useRef<ReturnType<typeof setTimeout> | undefined>(undefined)
   const isLongPress = useRef(false)
 
